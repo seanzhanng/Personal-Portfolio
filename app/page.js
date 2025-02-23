ab567bf80194b393e3cb7fb084f53445c733e599
Sean <snyoulin@gmail.com>
2025-02-23 16:24:10 -0500

    added introduction section

--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
+import HeroSection from "./components/HeroSection";
 
 export default function Home() {
   return (
-    <>
-
-      <h1> Hi! My name is Sean Zhang </h1>
-
-    </>
+    <main className="flex min-h-screen flex-col bg-black">
+      <div class="container mx-auto px-12 py-4">
+        <HeroSection />
+      </div>
+    </main>
   );
 }
