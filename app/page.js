af6d3fe14bceae6b1019b722f165ce6e209c97b5
Sean <snyoulin@gmail.com>
2025-02-28 21:32:01 -0500

    bugged version of infinite scroll

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col bg-black">
       <div class="container mx-auto px-12 py-4">
         <HeroSection />
-        {/* <Carousel /> */} {/* infinite scrolling */}
+        <Carousel /> {/* infinite scrolling */}
       </div>
     </main>
   );
