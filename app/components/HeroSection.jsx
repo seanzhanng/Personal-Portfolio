32c18b9549aa638d91c3f82fe665ba42cb0634a6
Sean <snyoulin@gmail.com>
2025-04-04 13:45:08 -0400

    added icons to socials

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -1,8 +1,9 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import NavLink from './Navlink';
+import Image from "next/image";
+import Link from "next/link";
+import NavLink from "./Navlink";
 
-const gradientText = "text-transparent bg-clip-text bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600";
+const gradientText =
+  "text-transparent bg-clip-text bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600";
 
 const HeroSection = () => {
   return (
@@ -11,22 +12,64 @@ const HeroSection = () => {
         <div className="col-span-7 place-self-center text-center sm:text-left">
           <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
             Hi! I'm {""}
-            <span className={gradientText}>
-              Sean Zhang
-            </span>
+            <span className={gradientText}>Sean Zhang</span>
           </h1>
           <p className="text-base sm:text-lg mb-6 lg:text-xl">
             <span className={gradientText}>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Welcome to my personal portfolio website!
             </span>
           </p>
-          <div className="sm:space-x-4 space-y-2 sm:space-y-0">
-            <button className="px-6 py-3 w-full sm:w-fit rounded-full bg-white hover:bg-slate-200 text-black">
-              About Me!
-            </button>
-            <button className="px-6 py-3 w-full sm:w-fit rounded-full bg-transparent hover:bg-slate-800 text-white border border-white">
-              Projects
-            </button>
+
+          <div className="flex flex-col sm:flex-row sm:items-center sm:space-x-4 space-y-2 sm:space-y-0">
+            <Link href="/about">
+              <button className="px-6 py-3 w-full sm:w-fit rounded-full bg-white hover:bg-slate-200 text-black">
+                About Me!
+              </button>
+            </Link>
+            <Link href="/projects">
+              <button className="px-6 py-3 w-full sm:w-fit rounded-full bg-transparent hover:bg-slate-800 text-white border border-white">
+                Projects
+              </button>
+            </Link>
+            <a
+              href="https://github.com/seanzhanng"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="/icons/github.svg"
+                alt="GitHub icon"
+                width={49.6}
+                height={49.6}
+                className="hover:opacity-80 transition"
+              />
+            </a>
+            <a
+              href="https://www.instagram.com/seanzhanng/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="/icons/instagram.svg"
+                alt="Instagram Icon"
+                width={49.6}
+                height={49.6}
+                className="hover:opacity-80 transition"
+              />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/sean-zhang-2a5769314"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="/icons/linkedin.svg"
+                alt="LinkedIn Icon"
+                width={49.6}
+                height={49.6}
+                className="hover:opacity-80 transition"
+              />
+            </a>
           </div>
         </div>
         <div className="col-span-5 flex justify-center">
@@ -41,7 +84,6 @@ const HeroSection = () => {
           </div>
         </div>
       </div>
-
     </section>
   );
 };
