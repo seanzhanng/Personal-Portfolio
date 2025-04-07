87fe229d80497ec597972c08148ae44a0f07b9e2
Sean <snyoulin@gmail.com>
2025-04-07 14:14:54 -0400

    changed LOGO to Home

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
     <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
       <div className="flex flex-wrap items-center justify-between mx-auto p-8">
         <Link href={"/"} className="text-2xl text-white font-semibold">
-          LOGO
+          Home
         </Link>
         <div className="menu md:block md:w-auto" id="navbar">
           <ul className="flex space-x-4">
